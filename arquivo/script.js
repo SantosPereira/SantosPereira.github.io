62adb989056e1f3e06185538265340e9504af8c6
SantosPereira <pedrohenriquelemam@gmail.com>
2021-09-09 06:56:01 -0300

    Alteração no projeto

--- arquivo/script.js
+++
@@ -1,4 +0,0 @@
-function pega() {
-    var dado = Document.getElementbyid("botão");
-    dado = Document.setElementbyid.setStyle("background-color='black';")
-}
---
+++ legado/v1/script.js
@@ -0,0 +1,4 @@
+function pega() {
+    var dado = Document.getElementbyid("botão");
+    dado = Document.setElementbyid.setStyle("background-color='black';")
+}
